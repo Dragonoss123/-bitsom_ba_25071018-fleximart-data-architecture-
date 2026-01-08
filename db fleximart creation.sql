c8bdb7b900ed6b978fe0d05dfd281f42a4b3db77
Aditya Srinath <78250044+Dragonoss123@users.noreply.github.com>
2026-01-08 21:49:23 +0530

    Update db fleximart creation.sql

--- db fleximart creation.sql
+++ db fleximart creation.sql
@@ -38,3 +38,6 @@ CREATE TABLE order_items (
     FOREIGN KEY (order_id) REFERENCES orders(order_id),
     FOREIGN KEY (product_id) REFERENCES products(product_id)
 );
+
+remove db fleximart creation.sql
+
